656a214bae7bd5dbe8e1c2d28f9d6f8df380be51
MiguelOrtiz <miguel.ortiz16@inacapmail.cl>
2023-11-02 14:39:19 -0400

    varios cambios

--- scrapping_app/app.py
+++ scrapping_app/app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template
-from funciones import scrap_tallarines
+from selenium_ejemplo import scrap_product_info
 
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/tallarines')
 def mostrar_precio_tallarines():
-    resultado = scrap_tallarines()
-    return render_template('precio_tallarines.html', resultado=resultado)
+    supermercados = ["Lider"]
+    producto = "Fideo Spaghetti N° 5 400 Gr"
+    urls = ["https://www.lider.cl/supermercado/product/sku/576170/Lider-Spaghetti-5"]
+    selectores = ['pdp-mobile-sales-price']
+    imagen_producto = ['https://dipy.cl/cdn/shop/products/cl_z108025_500x.jpg?v=1661811374']
+    
+    resultados = scrap_product_info(supermercados, producto, urls, selectores, imagen_producto)
+    
+    if resultados:  # Comprueba si la lista de resultados no esta vacia
+        return render_template('precio_tallarines.html', resultados=resultados)
+    else:
+        # Manejar el caso en que no se obtuvieron resultados
+        return "No se encontraron resultados"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
