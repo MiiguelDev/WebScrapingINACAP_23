379d50d66b85b7d78b2fa94f3ab4c37dec319850
MiguelOrtiz <miguel.ortiz16@inacapmail.cl>
2023-11-24 14:44:56 -0400

    mc

--- scrapping_app/descripcion.py
+++ scrapping_app/descripcion.py
@@ -7,8 +7,8 @@ descripciones = {
     'Lavalozas': 'Lavaloza Limón Con Bio Activos Quix, 500 Ml',
     'Agua': 'Agua Mineral Con Gas Cachantun, 1,6 L',
     'Te': 'Té Ceylán Premium Caja Té Supremo, 100 Un',
-    'Mantequilla': 'Mantequilla Sin Sal Colun, 250 G',
+    'Mantequilla': 'Mantequilla Para Untar Con Sal Colun,    250 Gr',
     'Leche' :'Leche Entera Natural Soprole, 1 L',
     'Lenteja': 'Lentejas 4 mm Bolsa Banquete, 1 Kg',
-    'Salchichas': 'Salchichas Sureñas (10 Un) Receta del Abuelo, 500 g'
+    'Salchichas': 'Salchichas (10 Un) Receta del Abuelo, 500 g'
 }
