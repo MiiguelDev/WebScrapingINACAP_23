f06e60e745e0e35d2ec0f43ae437219d76e88597
MiguelOrtiz <miguel.ortiz16@inacapmail.cl>
2023-11-27 14:04:08 -0300

    Act. general de modulos

--- utilitarios/test_con.py
+++ utilitarios/test_con.py
@@ -1,11 +1,7 @@
-# En este codigo se incluyen las consultas para 
-# 1.- Comprobar la conexion a la bd
-# 2.- Comprobar los registros en la bd
-# 3.- Comprobar la estructura de las tablas en la bd
-
 import mysql.connector
 from mysql.connector import Error
 
+# Crear conexión a la base de datos
 def create_db_connection(host, database, user, password):
     try:
         return mysql.connector.connect(
@@ -18,80 +14,85 @@ def create_db_connection(host, database, user, password):
         print("Error al conectar con MySQL:", e)
         return None
 
-def test_database_connection(host, database, user, password):
-    connection = create_db_connection(host, database, user, password)
+# Comprobar la conexión a la base de datos
+def test_database_connection(connection):
     if connection and connection.is_connected():
-        connection.close()
         return "Conexion exitosa con la base de datos."
     return False
 
-print(test_database_connection('3.22.156.122', 'comparacion_precios', 'scrapy', 'Inacap2023#'))
+# Ejecutar una consulta SQL y obtener los resultados
+def execute_query(connection, query):
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+    except Error as e:
+        print("Error al realizar la consulta:", e)
+    finally:
+        cursor.close()
 
-def fetch_all_data(host, database, user, password):
+# Fetch all data from all tables
+def fetch_all_data(connection):
+    # Diccionario de consultas para cada tabla
+    queries = {
+        "historial_precios": """
+        SELECT producto.producto_id, producto.producto_nombre, supermercado.super_nombre, historial_precios.precio, historial_precios.fecha_hora
+        FROM historial_precios
+        JOIN supermercado ON historial_precios.super_id = supermercado.super_id
+        JOIN producto ON historial_precios.producto_id = producto.producto_id
+        ORDER BY historial_precios.fecha_hora DESC
+        """,
+        "supermercado": "SELECT * FROM supermercado",
+        "producto": "SELECT * FROM producto",
+    }
+
+    for table_name, query in queries.items():
+        records = execute_query(connection, query)
+        if records:
+            print(f"\nDatos de {table_name}:")
+            for record in records:
+                print_record(table_name, record)
+        else:
+            print(f"No se encontraron registros en la tabla '{table_name}'.")
+
+# Print records with specific formatting
+def print_record(table_name, record):
+    if table_name == "historial_precios":
+        producto_id, producto_nombre, super_nombre, precio, fecha_hora = record
+        fecha_formateada = fecha_hora.strftime("%d-%m-%Y - %H:%M:%S")
+        print(f"{producto_id:<30}{producto_nombre:<30}{super_nombre:<30}{precio:<30}{fecha_formateada}")
+    else:
+        print(" ".join(str(r).ljust(10) for r in record))
+
+# Describe the structure of all tables
+def describe_all_tables_structure(connection):
+    tables = execute_query(connection, "SHOW TABLES")
+    for (table_name,) in tables:
+        print(f"\nEstructura de la tabla {table_name}:")
+        columns = execute_query(connection, f"DESCRIBE {table_name}")
+        for column in columns:
+            print(column)
+
+# Main execution
+if __name__ == "__main__":
+    host = '3.22.156.122'
+    database = 'comparacion_precios'
+    user = 'scrapy'
+    password = 'Inacap2023#'
+
     connection = create_db_connection(host, database, user, password)
-    if connection:
-        try:
-            cursor = connection.cursor()
+    
+    # Test Database Connection
+    print(test_database_connection(connection))
 
-            # Diccionario de consultas para cada tabla
-            queries = {
-                "historial_precios": """
-                SELECT producto.producto_id, producto.producto_nombre, supermercado.super_nombre, historial_precios.precio, historial_precios.fecha_hora
-                FROM historial_precios
-                JOIN supermercado ON historial_precios.super_id = supermercado.super_id
-                JOIN producto ON historial_precios.producto_id = producto.producto_id
-                ORDER BY historial_precios.fecha_hora DESC
-                """,
-                "supermercado": "SELECT * FROM supermercado",
-                "producto": "SELECT * FROM producto",
-            }
+    # Fetch and print all data from tables
+    if connection.is_connected():
+        fetch_all_data(connection)
+    
+    # Describe all tables
+    if connection.is_connected():
+        describe_all_tables_structure(connection)
 
-            # Ejecutar todas las consultas y manejar los resultados
-            for table_name, query in queries.items():
-                cursor.execute(query)
-                records = cursor.fetchall()
+    if connection.is_connected():
+        connection.close()
 
-                if records:
-                    print(f"\nDatos de {table_name}:")
-                    for record in records:
-                        if table_name == "historial_precios":
-                            # Formato específico para historial_precios
-                            producto_id, producto_nombre, super_nombre, precio, fecha_hora = record
-                            fecha_formateada = fecha_hora.strftime("%d-%m-%Y - %H:%M:%S")
-                            print(f"{producto_id:<30}{producto_nombre:<30}{super_nombre:<30}{precio:<30}{fecha_formateada}")
-                        else:
-                            # Formato genérico para las demás tablas
-                            print(" ".join(str(r).ljust(10) for r in record))
-                else:
-                    print(f"No se encontraron registros en la tabla '{table_name}'.")
-
-        except Error as e:
-            print("Error al realizar la consulta:", e)
-        finally:
-            cursor.close()
-            connection.close()
-
-# Reemplazar con tus propios detalles de conexión
-fetch_all_data('3.22.156.122', 'comparacion_precios', 'scrapy', 'Inacap2023#')
-
-
-def describe_all_tables_structure(host, database, user, password):
-    connection = create_db_connection(host, database, user, password)
-    if connection:
-        try:
-            cursor = connection.cursor()
-            cursor.execute("SHOW TABLES")
-            tables = cursor.fetchall()
-            for (table_name,) in tables:
-                print(f"\nEstructura de la tabla {table_name}:")
-                cursor.execute(f"DESCRIBE {table_name}")
-                columns = cursor.fetchall()
-                for column in columns:
-                    print(column)
-        except Error as e:
-            print("Error al realizar la consulta:", e)
-        finally:
-            cursor.close()
-            connection.close()
-
-describe_all_tables_structure('3.22.156.122', 'comparacion_precios', 'scrapy', 'Inacap2023#')
